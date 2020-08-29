599049af69278e3651e6c9800f253a4c3aaa1117
Crafting1i <67289898+Crafting1i@users.noreply.github.com>
2020-08-30 00:05:35 +0300

    Update JavaScript console log оповещения.js

--- JavaScript console log оповещения.js
+++ JavaScript console log оповещения.js
@@ -8,7 +8,7 @@ var style = ['padding: 1rem;',
              'background: linear-gradient( gold, orangered);',
              'text-shadow: 0 2px orangered;',
              'font: 1.3rem/3 Georgia;',
-             'color: white;'].join('');
+             'color: white;'];
 
 console.log ( '%c%s', style, str );
 
